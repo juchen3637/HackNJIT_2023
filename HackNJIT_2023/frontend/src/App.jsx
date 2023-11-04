4ec1323b334ffdea58ad596d1b4e23ab85375e70
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2023-11-04 13:50:27 -0400

    web app setup

---
+++ HackNJIT_2023/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>Hello world</header>
+      </div>
+    </>
+  )
+}
+
+export default App
