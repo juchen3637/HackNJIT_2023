bc6de730c9990afaa6131dd9981285d8e44827fc
Harris Pyo <HPW9229@gmail.com>
2023-11-04 17:59:00 -0400

    added displayInfo componenet as well as images to be used

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
+import DisplayInfo from "./components/DisplayInfo.jsx";
+import highTide from './images/high-tide.png';
 
 function App() {
+  let info = {
+    height: "1.18",
+    time: "2019-03-15 18:40:44+00:00",
+    type: "high"
+  };
+  const activity = 'fishing';
   return (
     <div className='bg-offwhite flex min-h-screen w-full'>
       <div className="flex justify-center bg-navy fixed left-0 right-0 h-10 py-2">
---
+++ frontend/src/components/DisplayInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { format } from 'date-fns';
+
+const DisplayInfo = ({data, tide, activity}) => {
+  let date = data.time.split(' ')[0].split('-');
+  let time = data.time.split(' ')[1];
+  time = time.split('+')[0];
+  time = time.split(':');
+  let midday = 'AM';
+  if (Number(time[0] > 12)) {
+    time[0] = (time[0] - 12).toString();
+    midday = 'PM';
+  }
+  return (
+    <div className='flex w-[300px] p-2 text-white bg-navy rounded-lg'>
+      <div className='flex flex-2 flex-col mr-8 w-44'>
+        <div className='text-center'>{format(new Date(date), 'MM/dd/yyyy')} &nbsp; {time[0]}:{time[1]} {midday}</div>
+        <div className='text-xl font-bold my-3 text-center'>{data.type.charAt(0).toUpperCase() + data.type.slice(1)}</div>
+        <div className='text-sm mb-3 text-center'>Suggested Activity: {activity}</div>
+      </div>
+      <div className='items-center justify-center flex'>
+        <img className='flex-1 w-16 h-16 rounded-full bg-white' src={tide} alt="Tide image" />
+      </div>
+    </div>
+  )
+}
+
+export default DisplayInfo
