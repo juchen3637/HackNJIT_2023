7d9375fcf11c972a1ba463bf9a141bf7163a3d69
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2023-11-04 14:17:51 -0400

    Tailwind working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <header>Hello world</header>
-      </div>
-    </>
+    <h1 className="text-3xl font-bold underline text-red-900">
+      Hello world!
+    </h1>
   )
 }
 
